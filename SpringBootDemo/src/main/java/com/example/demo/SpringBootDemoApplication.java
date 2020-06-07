e1462a7cc401970da68c75f4545a3d41a9ebdd1c
lokes <rani.verla@gmail.com>
2020-06-07 14:37:22 -0700

    added some loggers

--- SpringBootDemo/src/main/java/com/example/demo/SpringBootDemoApplication.java
+++ SpringBootDemo/src/main/java/com/example/demo/SpringBootDemoApplication.java
@@ -1,16 +1,23 @@
 package com.example.demo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 @EnableEurekaServer
 public class SpringBootDemoApplication {
+	
+	
+	static Logger log=LoggerFactory.getLogger(SpringBootDemoApplication.class);
 	
 	@GetMapping("/welcome")
     public String getMessage()
@@ -19,8 +26,17 @@ public class SpringBootDemoApplication {
     }
 	
 	
+	@PostConstruct
+	public void init()
+	{
+		log.info("application started");
+	}
+	
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
+		log.info("Application Executed");
 	}
 
 }
